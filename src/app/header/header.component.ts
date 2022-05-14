8e02f14ada0eace703d86696a2919253cec84c05
mata0050 <mata0050@outlook.com>
2022-05-14 11:06:14 -0400

    Navigation setup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  collapsed: boolean = false;
+  constructor() {}
+  ngOnInit(): void {}
+}
